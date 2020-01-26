e00932eaef853a27934e148847fa95d7b7722199
adobkin7 <dobs@u.northwestern.edu>
2020-01-26 12:06:37 -0800

    Updates

--- precache-manifest.95a9c7ffd2493534a3cf8e72b6a1c6c7.js
+++ precache-manifest.95a9c7ffd2493534a3cf8e72b6a1c6c7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "131a4a471ed6c7ff997c735c855c3c88",
+    "revision": "039151e3db4a2fa073b6b94c6924eca0",
     "url": "/scavenger-hunt-2020/index.html"
   },
   {
-    "revision": "0c13d298ca6948559ba5",
+    "revision": "b6069b96f784188f4e3f",
     "url": "/scavenger-hunt-2020/static/css/main.0fb23da4.chunk.css"
   },
   {
-    "revision": "e1685b3f750478b4cc8d",
-    "url": "/scavenger-hunt-2020/static/js/2.6c42c317.chunk.js"
+    "revision": "d09846a52725267a8cc6",
+    "url": "/scavenger-hunt-2020/static/js/2.5ab94977.chunk.js"
   },
   {
     "revision": "3e6887b9ed6d8bee1d899d2295e5baa1",
-    "url": "/scavenger-hunt-2020/static/js/2.6c42c317.chunk.js.LICENSE"
+    "url": "/scavenger-hunt-2020/static/js/2.5ab94977.chunk.js.LICENSE"
   },
   {
-    "revision": "0c13d298ca6948559ba5",
-    "url": "/scavenger-hunt-2020/static/js/main.f5539034.chunk.js"
+    "revision": "b6069b96f784188f4e3f",
+    "url": "/scavenger-hunt-2020/static/js/main.f9a2cb7b.chunk.js"
   },
   {
     "revision": "7bbc576e2e4dd3037c66",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scavenger-hunt-2020/precache-manifest.b9c23cbe89696a730b4a79d7a7b8cacc.js"
+  "/scavenger-hunt-2020/precache-manifest.95a9c7ffd2493534a3cf8e72b6a1c6c7.js"
 );
 
 self.addEventListener('message', (event) => {
