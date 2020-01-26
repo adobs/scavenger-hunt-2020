72e6a04cd60641ee22a15c6a2d8cc3199902f362
adobkin7 <dobs@u.northwestern.edu>
2020-01-26 11:32:28 -0800

    Updates

---
+++ precache-manifest.b9c23cbe89696a730b4a79d7a7b8cacc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131a4a471ed6c7ff997c735c855c3c88",
+    "url": "/scavenger-hunt-2020/index.html"
+  },
+  {
+    "revision": "0c13d298ca6948559ba5",
+    "url": "/scavenger-hunt-2020/static/css/main.0fb23da4.chunk.css"
+  },
+  {
+    "revision": "e1685b3f750478b4cc8d",
+    "url": "/scavenger-hunt-2020/static/js/2.6c42c317.chunk.js"
+  },
+  {
+    "revision": "3e6887b9ed6d8bee1d899d2295e5baa1",
+    "url": "/scavenger-hunt-2020/static/js/2.6c42c317.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c13d298ca6948559ba5",
+    "url": "/scavenger-hunt-2020/static/js/main.f5539034.chunk.js"
+  },
+  {
+    "revision": "7bbc576e2e4dd3037c66",
+    "url": "/scavenger-hunt-2020/static/js/runtime-main.4b296607.js"
+  }
+]);
